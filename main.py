21ff3f886185877cb299b93b6afbe8a711173cbe
Mustafa Kaptanlar <46494143+mustafakaptanlar@users.noreply.github.com>
2023-01-16 01:27:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ if choice == "density":
     subprocess.call(["python", "Formulas/Density.py"]) #Calls Density Formula
 if choice == "average speed":
     subprocess.call(["python", "Formulas/Average Speed.py"]) #Calls Average Speed Formula
+if choice == "force":
+    subprocess.call(["python", "Formulas/Newton's Second Law.py"]) #Calls Newton's Second Law Formula
+if choice == "power":
+    subprocess.call(["python", "Formulas/Power.py"]) #Calls Power Formula
+if choice == "weight":
+    subprocess.call(["python", "Formulas/Weight.py"]) #Calls Weight Formula
