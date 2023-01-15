2e8dc5cc9d2cb08bdbc571e6dec78456b442ae5e
Mustafa Kaptanlar <46494143+mustafakaptanlar@users.noreply.github.com>
2023-01-16 00:27:16 +0300

    Formula Added
    Acceleration Formula

---
+++ Formulas/Acceleration.py
@@ -0,0 +1,7 @@
+print("Accelaration Formula")
+initial_velocity = float(input("Initial velocity : "))
+final_velocity = float(input("Final velocity : "))
+time = float(input("Time taken: "))
+velocity_difference = final_velocity - initial_velocity
+accelaration = velocity_difference / time
+print("Accelaration : ",accelaration)
