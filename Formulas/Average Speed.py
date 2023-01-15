397ffa150b76f53363673b731108d279301191d4
Mustafa Kaptanlar <46494143+mustafakaptanlar@users.noreply.github.com>
2023-01-16 00:13:50 +0300

    Add files via upload

---
+++ Formulas/Average Speed.py
@@ -0,0 +1,5 @@
+print("average speed formula")
+distance = float(input("Total distance traveled: "))
+time = float(input("Total time is taken: "))
+average_speed = distance / time
+print("average speed : ", average_speed)
