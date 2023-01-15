a5e017a4e72c0db6f3e73e31f2194b8b3e0d6697
Mustafa Kaptanlar <46494143+mustafakaptanlar@users.noreply.github.com>
2023-01-16 01:08:32 +0300

    Update main.py
    Added new commands.

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-import subprocess
-subprocess.call(["python", "Formulas\Acceleration.py"])
+import subprocess #import library
+
+choice = input("What you want to calculate : ") #user choices which calculation he/she wants to do.
+
+if choice == "acceleration":
+    subprocess.call(["python", "Formulas/Acceleration.py"]) #Calls Acceleration Formula
+if choice == "density":
+    subprocess.call(["python", "Formulas/Density.py"]) #Calls Density Formula
+if choice == "average speed":
+    subprocess.call(["python", "Formulas/Average Speed.py"]) #Calls Average Speed Formula
