2916bae103d466a2956d895c82781a893b468215
Mustafa Kaptanlar <46494143+mustafakaptanlar@users.noreply.github.com>
2023-01-16 01:24:03 +0300

    File added
    Newton's Second Law Formula

---
+++ Formulas/Newton's Second Law.py
@@ -0,0 +1,5 @@
+print("Newton's Second Law Formula")
+mass = float(input("Mass of the body : "))
+acceleration = float(input("Acceleration : "))
+force = mass * acceleration
+print("Force : ", force)
