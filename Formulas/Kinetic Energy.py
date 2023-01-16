3acf14dda3f8496ff83b9d03ea31a6e8bb3a9531
Mustafa Kaptanlar <46494143+mustafakaptanlar@users.noreply.github.com>
2023-01-16 21:21:18 +0300

    Update Kinetic Energy.py
    Formula added.

--- Formulas/Kinetic Energy.py
+++ Formulas/Kinetic Energy.py
@@ -1,4 +1,5 @@
 print("Kinetic Energy Formula")
+print("E = 1/2 x m x v^2")
 half = 1/2
 mass = float(input("Mass of the body : "))
 velocity = float(input("The velocity with which the body is travelling : "))
