0ce209f9a03faa0c0c287086be34716ccba13b81
Mustafa Kaptanlar <46494143+mustafakaptanlar@users.noreply.github.com>
2023-01-16 21:06:21 +0300

    File added
    Frequency Formula and Ohm's Law Formula

---
+++ Formulas/Frequency Formula.py
@@ -0,0 +1,6 @@
+print("Frequency Formula\n")
+print("f = V / Lamda\n")
+velocity = float(input("Velocity : "))
+wavelength = float(input("Wavelength : "))
+frequency = velocity / wavelength
+print("Frequency : ", frequency)
---
+++ Formulas/Ohm's Law.py
@@ -0,0 +1,6 @@
+print("Ohm's Law Formula\n")
+print("V=IxR\n")
+current = float(input("Current : "))
+resistance = float(input("Resitance : "))
+voltage = current * resistance
+print("Voltage : ", voltage)
